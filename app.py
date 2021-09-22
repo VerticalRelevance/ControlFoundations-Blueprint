b99f80ecd7e4ba38c3a9289daf09972ae8631b5a
Eddie Peters <epeters@verticalrelevance.com>
2021-09-22 16:05:18 -0400

    Make controls pipeline into a CDK construct

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-from controls_pipeline.controls_scripts.controls import *
 import os
 
 from aws_cdk import core as cdk
@@ -8,20 +7,35 @@ from controls_pipeline.controls_pipeline_stack import ControlsPipelineStack
 
 app = cdk.App()
 
+common_env = cdk.Environment(
+    account=os.environ.get("CDK_DEFAULT_ACCOUNT"), region=os.environ.get("CDK_DEFAULT_REGION")
+)
+
 # Input parameters. Update according to your existing github owner name and repo names.
 github_owner = "showley-vr"
 controls_repo = "ControlsFoundation-ControlsPipeline"
 application_repo = "ControlsFoundation-ApplicationPipeline"
 
 # Initialize the stacks.
-controls_stack = ControlsPipelineStack(app, "ControlsFoundationControlsPipeline", github_owner, controls_repo)
-application_stack = ControlsPipelineStack(app, "ControlsFoundationTestApplicationPipeline", github_owner, application_repo)
+controls_stack = ControlsPipelineStack(
+    app,
+    "ControlsFoundationControlsPipeline",
+    github_owner,
+    controls_repo,
+    env=common_env
+)
+# application_stack = ControlsPipelineStack(
+#    app,
+#    "ControlsFoundationTestApplicationPipeline",
+#    github_owner,
+#    application_repo,
+#    env=common_env,
+# )
 
-# Create the pipelines.
-controls_stack.deploy_pipeline()
-application_stack.deploy_pipeline()
-
-# Deploy security controls.
-controls_stack.deploy_controls()
+## Create the pipelines.
+# controls_stack.deploy_pipeline()
+#
+## Deploy security controls.
+# controls_stack.deploy_controls()
 
 app.synth()
--- controls_pipeline/controls_pipeline_stack.py
+++ controls_pipeline/controls_pipeline_stack.py
@@ -1,67 +1,237 @@
-# Import aws_cdk packages.
+import os
+
+import boto3
+import jsii
 from aws_cdk import (
     core as cdk,
     aws_codepipeline as codepipeline,
     aws_codepipeline_actions as codepipeline_actions,
     pipelines as pipelines,
-    aws_codestarconnections as codestarconnections
+    aws_codestarconnections as codestarconnections,
+    aws_s3,
+    aws_s3_assets,
+    aws_lambda,
+    aws_config,
+    aws_guardduty,
+    aws_macie,
+    aws_accessanalyzer,
+)
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+SUPPLEMENTARY_FILES_DIR = os.path.join(CURRENT_DIR, "../supplementary_files")
+DEFAULT_CONFORMANCE_PACK_FILE_PATH = os.path.join(
+    SUPPLEMENTARY_FILES_DIR, "s3-guardrails-conformance-pack.yaml"
+)
+DEFAULT_GUARDDUTY_THREAT_INTEL_SCRIPT_PATH = os.path.join(
+    SUPPLEMENTARY_FILES_DIR, "threat-intel-my-ip.txt"
 )
 
-from controls_scripts.controls import *
+
+@jsii.implements(cdk.IAspect)
+class S3BucketPublicAccessOffAspect:
+    def visit(self, node):
+        if isinstance(node, aws_s3.Bucket):
+            node._disallow_public_access = True
+
 
 class ControlsPipelineStack(cdk.Stack):
-
-    # Define init method.
-    def __init__(self, scope: cdk.Construct, id: str, github_repo_owner: str, github_repo_name: str, **kwargs) -> None:
+    def __init__(
+        self,
+        scope: cdk.Construct,
+        id: str,
+        github_repo_owner: str,
+        github_repo_name: str,
+        local_guardduty_threat_intel_set_path: str = DEFAULT_GUARDDUTY_THREAT_INTEL_SCRIPT_PATH,
+        local_conformance_pack_path: str = DEFAULT_CONFORMANCE_PACK_FILE_PATH,
+        **kwargs,
+    ) -> None:
         super().__init__(scope, id, **kwargs)
         self.github_repo_owner = github_repo_owner
-        self.github_repo_name=github_repo_name
+        self.github_repo_name = github_repo_name
+        self.local_guardduty_threat_intel_set_path = (
+            local_guardduty_threat_intel_set_path
+        )
+        self.local_conformance_pack_path = local_conformance_pack_path
 
-    def deploy_pipeline(self):        
+        self.configure_utility_s3_bucket()
+        self.configure_pipeline()
+
+        self.configure_config_conformance_pack()
+        self.configure_config_custom_rules()
+        self.configure_guardduty()
+        self.configure_iam_access_analyzer()
+        self.configure_macie()
+
+        # Turn off public access on any buckets created in this stack
+        cdk.Aspects.of(self).add(S3BucketPublicAccessOffAspect())
+
+    def configure_pipeline(self):
         # Create codestar connection to connect pipeline to git.
         connection_name = "".join((self.github_repo_name, "_git_connection"))
-        pipeline_git_connection = codestarconnections.CfnConnection(self, connection_name, 
-            connection_name="git_connection_props", 
-            provider_type="GitHub"
-        )   
+        pipeline_git_connection = codestarconnections.CfnConnection(
+            self,
+            connection_name,
+            connection_name=connection_name,
+            provider_type="GitHub",
+        )
 
         # Define the artifacts that represent source code and cloud assembly.
         pipeline_source_artifact = codepipeline.Artifact()
         pipeline_cloud_assembly_artifact = codepipeline.Artifact()
 
         # Define pipeline source action.
-        git_connection_arn = pipeline_git_connection.get_att("ConnectionArn").to_string()
+        git_connection_arn = pipeline_git_connection.get_att(
+            "ConnectionArn"
+        ).to_string()
+
         pipeline_source_action = codepipeline_actions.CodeStarConnectionsSourceAction(
             action_name="GitHub_Source",
             connection_arn=git_connection_arn,
             output=pipeline_source_artifact,
             owner=self.github_repo_owner,
-            repo=self.github_repo_name
+            repo=self.github_repo_name,
         )
 
         # Define pipeline synth action.
         pipeline_synth_action = pipelines.SimpleSynthAction(
             install_commands=[
-                "npm install -g aws-cdk", # Installs the cdk cli on Codebuild
-                "pip install -r requirements.txt" # Instructs Codebuild to install required packages
+                "npm install -g aws-cdk",  # Installs the cdk cli on Codebuild
+                "pip install -r requirements.txt",  # Instructs Codebuild to install required packages
             ],
             synth_command="npx cdk synth",
-            source_artifact=pipeline_source_artifact, # Where to get source code to build
-            cloud_assembly_artifact=pipeline_cloud_assembly_artifact, # Where to place built source
+            source_artifact=pipeline_source_artifact,  # Where to get source code to build
+            cloud_assembly_artifact=pipeline_cloud_assembly_artifact,  # Where to place built source
         )
 
         # Create the pipeline.
-        pipeline = pipelines.CdkPipeline(
-            self, "Pipeline",
-            cloud_assembly_artifact = pipeline_cloud_assembly_artifact,
-            source_action = pipeline_source_action,
-            synth_action = pipeline_synth_action
+        self.pipeline = pipelines.CdkPipeline(
+            self,
+            "Pipeline",
+            cloud_assembly_artifact=pipeline_cloud_assembly_artifact,
+            source_action=pipeline_source_action,
+            synth_action=pipeline_synth_action,
         )
 
-    def deploy_controls(self):
-        # Deploy conformance pack.
-        deploy_config_conformance_pack(self)
-        deploy_config_custom_rules(self)
-        deploy_guardduty(self)
-        deploy_macie(self)
-        deploy_iam_access_analyzer(self)
+    def configure_utility_s3_bucket(self):
+        """Create an S3 bucket to store various stack assets in.
+
+        Prevents us from creating tons of buckets for this one stack."""
+        self.utility_s3_bucket = aws_s3.Bucket(self, "UtilityS3Bucket")
+
+    def configure_config_conformance_pack(
+        self,
+    ):
+        self.conformance_pack_asset = aws_s3_assets.Asset(
+            self, "ConformancePackAsset", path=self.local_conformance_pack_path
+        )
+
+        self.config_conformance_pack = aws_config.CfnConformancePack(
+            self,
+            "Controls Foundation Test Pack",
+            conformance_pack_name="s3-guardrails-conformance-pack",
+            template_s3_uri=self.conformance_pack_asset.s3_url,
+        )
+
+    def configure_config_custom_rules(self):
+        # Create lambda for custom config rule (detective).
+        self.config_detective_rule_lambda = aws_lambda.Function(
+            self,
+            "Config-Rule-Detective",
+            runtime=aws_lambda.Runtime.PYTHON_3_7,
+            code=aws_lambda.Code.asset("controls_pipeline/controls_scripts/lambdas"),
+            handler="config_custom_rule_detective.handler",
+        )
+        # Create lambda for custom config rule (reactive).
+        self.config_reactive_rule_lambda = aws_lambda.Function(
+            self,
+            "Config-Rule-Reactive",
+            runtime=aws_lambda.Runtime.PYTHON_3_7,
+            code=aws_lambda.Code.asset("controls_pipeline/controls_scripts/lambdas"),
+            handler="config_custom_rule_reactive.handler",
+        )
+
+        # Create custom config rule (detective).
+        self.config_detective_rule_scope = aws_config.RuleScope.from_tag(
+            key="TEST-EC2_CUSTOM-CONFIG-RULE"
+        )
+        self.config_detective_rule = aws_config.CustomRule(
+            self,
+            "Detect EC2 Tag Violation",
+            description="Detect when EC2 instances do not have an 'sdlc_env' tag attached",
+            config_rule_name="DetectEc2TagViolation",
+            lambda_function=self.config_detective_rule_lambda,
+            rule_scope=self.config_detective_rule_scope,
+            configuration_changes=True,
+            periodic=True,
+        )
+
+        # Create custom config rule (detective).
+        self.config_reactive_rule_scope = aws_config.RuleScope.from_tag(
+            key="TEST-EC2_CUSTOM-CONFIG-RULE"
+        )
+        self.config_reactive_rule = aws_config.CustomRule(
+            self,
+            "React to EC2 Tag Violation",
+            description="React to when EC2 instances do not have an 'sdlc_env' tag attached",
+            config_rule_name="ReactToEc2TagViolation",
+            lambda_function=self.config_reactive_rule_lambda,
+            configuration_changes=True,
+            periodic=True,
+        )
+
+    def configure_guardduty(self):
+        # Create GuardDuty findings bucket.
+        self.guardduty_findings_bucket = aws_s3.Bucket(
+            self, "Controls-Foundation-GuardDuty-Findings"
+        )
+        # Create GuardDuty threat-intel-scripts bucket.
+        self.guardduty_findings_bucket = aws_s3.Bucket(
+            self, "Controls-Foundation-GuardDuty-Threat-Intel-Scripts"
+        )
+        # Create account-level GuardDuty detector.
+        self.guardduty_detector = aws_guardduty.CfnDetector(
+            self,
+            "Baseline GuardDuty Detector",
+            enable=True,
+            finding_publishing_frequency="ONE_HOUR",
+        )
+        self.guardduty_threat_intel_asset = aws_s3_assets.Asset(
+            self,
+            "GuardDutyThreatIntelAsset",
+            path=self.local_guardduty_threat_intel_set_path,
+        )
+
+        self.guardduty_threat_intel_set = aws_guardduty.CfnThreatIntelSet(
+            self,
+            "My IP Threat Intel Set",
+            activate=True,
+            detector_id=self.guardduty_detector.ref,
+            format="TXT",
+            location=self.guardduty_threat_intel_asset.s3_url,
+        )
+
+    def configure_macie(self):
+        # Create GuardDuty findings bucket.
+        self.macie_findings_bucket = aws_s3.Bucket(
+            self, "Controls-Foundation-Macie-Findings"
+        )
+        # Start Macie session
+        self.macie_session = aws_macie.CfnSession(
+            self,
+            "Test Macie Session",
+            finding_publishing_frequency="FIFTEEN_MINUTES",
+            status="ENABLED",
+        )
+
+        self.macie_custom_data_identifier = aws_macie.CfnCustomDataIdentifier(
+            self,
+            "Controls Foundation Macie",
+            description="This is a Macie custom identifier that PID data in S3 buckets.",
+            name="controls-foundation-macie-test-pid",
+            regex="(\d){3}-(\d){2}-(\d){4}",
+        )
+
+    def configure_iam_access_analyzer(self):
+        # Create an account-level analyzer
+        self.account_analyzer = aws_accessanalyzer.CfnAnalyzer(
+            self, "Account IAM Access Analyzer", type="ACCOUNT"
+        )
